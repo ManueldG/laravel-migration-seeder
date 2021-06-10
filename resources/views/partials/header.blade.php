66ed8946a90ec0ab90815b28d1da8df2dcc56ae3
Manuel della Gala <manuel.dellagala@gmail.com>
2021-06-10 23:31:54 +0200

    ok

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css' integrity='sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA==' crossorigin='anonymous'/>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -13,5 +15,15 @@
     </head>
     <body>
         <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="{{ route('main')}}">HOME</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('contatti')}}">CONTATTI</a>
+                    </li>
+                </ul>
+            </nav>
 
         </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('main');
+})->name('main');
+
+Route::get('/contatti', function () {
+    return view('contatti');
+})->name('contatti');
