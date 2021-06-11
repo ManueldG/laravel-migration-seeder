82ac108109a7a945efcaf8d7f3c05c6762335e65
Manuel della Gala <manuel.dellagala@gmail.com>
2021-06-11 10:15:50 +0200

    ok

--- resources/views/voyages.blade.php
+++ resources/views/voyages.blade.php
@@ -8,18 +8,23 @@
             Voyages
         </h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quia corporis quam, ex officia perferendis reiciendis minus molestiae! Omnis veniam aut libero mollitia, incidunt assumenda expedita atque ipsa accusamus est!</p>
-        @foreach ($voyages as $voyage)
+        <div class="flex">
+            @foreach ($voyages as $voyage)
 
+                <article>
 
-        {{ $voyage->location }}
-        {{ $voyage->days }}
-        {{ $voyage->price }}
-        {{ $voyage->place }}
-        {{ $voyage->treatment }}
-        {{ $voyage->description }}
+                    <h2>{{ $voyage->location }}</h2>
+                    <h3>{{ $voyage->description }}</h3>
+                    {{ $voyage->days }} giorni
+                    {{ $voyage->price }} euro a persona
+                    sistemazione in {{ $voyage->place }}
+                    trattamento {{ $voyage->treatment }}
 
+                </article>
 
         @endforeach
+        </div>
+
     </div>
 
 @endsection
